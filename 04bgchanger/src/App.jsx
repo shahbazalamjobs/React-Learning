3c2eb5bdf2b5c0fc0eafbaf26fa2b47e37476aa0
Shahbaz Alam <shahbazalamjobs@gmail.com>
2024-02-12 11:11:06 +0530

    Add files via upload

---
+++ 04bgchanger/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [color, setColor] = useState("olive")
+
+  return (
+    <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded'>
+          <button onClick={() => { setColor("red") }} className='outline-none px-2 py-1 rounded text-white shadow-l' style={{ backgroundColor: "red" }}> Red </button>
+          <button onClick={() => { setColor("green") }} className='outline-none px-2 py-1 rounded text-white shadow-l' style={{ backgroundColor: "green" }}> Green </button>
+          <button onClick={() => { setColor("yellow") }} className='outline-none px-4 py-2 rounded text-white shadow-l' style={{ backgroundColor: "yellow", color: "black" }}> Yellow </button>
+          <button onClick={() => { setColor("blue") }} className='outline-none px-4 py-2 rounded text-white shadow-l' style={{ backgroundColor: "blue" }}> Blue </button>
+          <button onClick={() => { setColor("black") }} className='outline-none px-4 py-2 rounded text-white shadow-l' style={{ backgroundColor: "black" }}> Black </button>
+          <button onClick={() => { setColor("olive") }} className='outline-none px-4 py-2 rounded text-white shadow-l' style={{ backgroundColor: "olive" }}> Olive </button>
+          <button onClick={() => { setColor("purple") }} className='outline-none px-4 py-2 rounded text-white shadow-l' style={{ backgroundColor: "purple" }}> Purple </button>
+          <button onClick={() => { setColor("pink") }} className='outline-none px-4 py-2 rounded text-white shadow-l' style={{ backgroundColor: "pink" }}> Pink </button>
+          <button onClick={() => { setColor("lavender") }} className='outline-none px-4 py-2 rounded text-white shadow-l' style={{ backgroundColor: "lavender", color: "black" }}> Lavender </button>
+        </div>
+      </div>
+    </div >
+  )
+}
+
+export default App
